259d8171222a4ff050cbe1eebd0a96b8ade11966
AndreBurkett <andre.burkett@gmail.com>
2017-12-01 01:51:32 -0700

    update build priority

--- dev/src/task.build.js
+++ dev/src/task.build.js
@@ -1,7 +1,12 @@
 "use strict";
 var taskBuild = {
     run: function (creep) {
-        var target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+        var target;
+        var containerPrint = creep.room.find(FIND_CONSTRUCTION_SITES, { filter: function (s) { return s.structureType === STRUCTURE_CONTAINER; } });
+        if (containerPrint && containerPrint.length > 0)
+            target = creep.pos.findClosestByRange(containerPrint);
+        else
+            target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
         if (creep.build(target) == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
         }
--- src/task.build.ts
+++ src/task.build.ts
@@ -1,7 +1,13 @@
 var taskBuild = {
     run: function(creep: Creep) {
+        let target
         //target = Game.getObjectById(creep.memory.buildTarget);
-        let target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+        let containerPrint = creep.room.find(FIND_CONSTRUCTION_SITES, {filter: (s: Structure) => s.structureType === STRUCTURE_CONTAINER});
+        if (containerPrint && containerPrint.length > 0)
+        target = creep.pos.findClosestByRange(containerPrint);
+        else
+        target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+
         if (creep.build(target) == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
         }
