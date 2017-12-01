0b07d90c749a5cc143f12ef4016e75b0f800923b
AndreBurkett <andre.burkett@gmail.com>
2017-12-01 01:04:05 -0700

    delete new folder

--- src/task.transport.ts
+++ src/task.transport.ts
@@ -1,7 +1,7 @@
 var taskTransport = {
     run: function(creep) {
         let ctrl = creep.room.controller;
-        let target = ctrl.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.structureType === STRUCTURE_CONTAINER && s.memory && s.memory.transportTarget == true});
+        let target = ctrl.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s: Structure) => s.structureType === STRUCTURE_CONTAINER && s.memory && s.memory.transportTarget == true});
 
         if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
