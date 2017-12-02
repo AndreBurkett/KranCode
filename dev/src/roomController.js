a517c41d10b55da1e25b9826c8b3d0d4a8bcf39c
AndreBurkett <andre.burkett@gmail.com>
2017-12-02 03:47:53 -0700

    debug

--- dev/src/roomController.js
+++ dev/src/roomController.js
@@ -84,8 +84,7 @@ function roomController(room) {
         AssignTask('withdraw', 2, 'harvest');
     let wCreeps = room.find(FIND_MY_CREEPS, { filter: (c) => c.memory.task === 'withdraw' && c.carry[RESOURCE_ENERGY] === c.carryCapacity });
     for (let i in wCreeps) {
-        if (wCreeps[i].memory.taskQ)
-            wCreeps[i].memory.task = wCreeps.memory.taskQ;
+        wCreeps[i].memory.task = wCreeps[i].memory.taskQ;
         delete wCreeps[i].memory.taskQ;
     }
     let iCreeps = room.find(FIND_MY_CREEPS, { filter: (c) => c.carry[RESOURCE_ENERGY] === 0 && c.memory.task === 'build' || c.memory.task === 'harvest' || c.memory.task === 'repair' || c.memory.task === 'transport' || c.memory.task === 'upgrade' });
--- src/roomController.ts
+++ src/roomController.ts
@@ -116,8 +116,7 @@ function roomController(room: Room) {
     //Assign Withdraw Task
     let wCreeps = room.find(FIND_MY_CREEPS, {filter: (c: Creep) => c.memory.task === 'withdraw' && c.carry[RESOURCE_ENERGY] === c.carryCapacity});
     for(let i in wCreeps){
-        if(wCreeps[i].memory.taskQ)
-        wCreeps[i].memory.task = wCreeps.memory.taskQ;
+        wCreeps[i].memory.task = wCreeps[i].memory.taskQ;
         delete wCreeps[i].memory.taskQ;
     }
 
