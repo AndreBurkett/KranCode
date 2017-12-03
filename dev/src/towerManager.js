c76620af5719af05c0963912e0ba6d0ad292b11e
AndreBurkett <andre.burkett@gmail.com>
2017-12-03 05:16:13 -0700

    tower Implementatioion

--- dev/src/towerManager.js
+++ dev/src/towerManager.js
@@ -1,8 +1,11 @@
 "use strict";
-run: function (t) {
-    console.log('tm run');
-    let hostile = t.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    if (hostile)
-        t.attack(hostile);
-}
+var towerManager = {
+    run: function (t) {
+        console.log('tm run');
+        let hostile = t.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (hostile)
+            t.attack(hostile);
+    }
+};
+module.exports = towerManager;
 //# sourceMappingURL=towerManager.js.map
--- src/towerManager.ts
+++ src/towerManager.ts
@@ -10,10 +10,14 @@
     }
 }
 */
+var towerManager = {
+    run: function (t: StructureTower) {
+        console.log('tm run');
+        let hostile = t.pos.findClosestByRange<Creep>(FIND_HOSTILE_CREEPS)
+        if (hostile)
+            t.attack(hostile);
+    }
 
-run: function(t: StructureTower){
-    console.log('tm run');
-    let hostile = t.pos.findClosestByRange<Creep>(FIND_HOSTILE_CREEPS)
-    if(hostile)
-        t.attack(hostile);
 }
+
+module.exports = towerManager;
