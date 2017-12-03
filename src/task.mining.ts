3fda4f1f29dcf8f795a5de055d7c975c27012440
AndreBurkett <andre.burkett@gmail.com>
2017-12-02 23:26:53 -0700

    update mining logic

--- src/task.mining.ts
+++ src/task.mining.ts
@@ -2,6 +2,12 @@ var taskMine = {
     run: function(creep: Creep) {
         var target;
         target = Game.getObjectById(creep.memory.target);
+        if (!creep.memory.taskQ) {
+            if (creep.pos.findInRange(STRUCTURE_CONTAINER, 3).length > 0)
+                creep.memory.taskQ = 'deposit';
+            else
+                creep.memory.taskQ = 'build';
+        }
         if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
         }
