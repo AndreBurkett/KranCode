31084cac22ac04bd2caa5b907edc86cf9f746380
AndreBurkett <andre.burkett@gmail.com>
2017-12-06 02:55:28 -0700

    prioritize extensions

--- dev/src/task.build.js
+++ dev/src/task.build.js
@@ -5,8 +5,15 @@ var taskBuild = {
         let containerPrint = creep.room.find(FIND_CONSTRUCTION_SITES, { filter: (s) => s.structureType === STRUCTURE_CONTAINER });
         if (containerPrint && containerPrint.length > 0)
             target = creep.pos.findClosestByRange(containerPrint);
-        else
-            target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+        else {
+            let extensionPrint = creep.room.find(FIND_CONSTRUCTION_SITES, { filter: (s) => s.structureType === STRUCTURE_EXTENSION });
+            if (extensionPrint && extensionPrint.length > 0) {
+                target = creep.pos.findClosestByRange(extensionPrint);
+            }
+            else {
+                target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+            }
+        }
         if (!target)
             creep.memory.task = 'idle';
         if (creep.build(target) == ERR_NOT_IN_RANGE) {
--- src/task.build.ts
+++ src/task.build.ts
@@ -4,9 +4,17 @@ var taskBuild = {
         //target = Game.getObjectById(creep.memory.buildTarget);
         let containerPrint = creep.room.find(FIND_CONSTRUCTION_SITES, {filter: (s: Structure) => s.structureType === STRUCTURE_CONTAINER});
         if (containerPrint && containerPrint.length > 0)
-        target = creep.pos.findClosestByRange(containerPrint);
-        else
-        target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+            target = creep.pos.findClosestByRange(containerPrint);
+        else{
+            let extensionPrint = creep.room.find(FIND_CONSTRUCTION_SITES, {filter: (s: Structure) => s.structureType === STRUCTURE_EXTENSION});
+            if(extensionPrint && extensionPrint.length > 0){
+                target = creep.pos.findClosestByRange(extensionPrint);
+            }
+            else{
+                target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+            }
+        }
+
         if(!target)
             creep.memory.task = 'idle';
 
