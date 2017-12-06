5bbbb42fb8e6d5026495c975b7b2e97e67e43510
AndreBurkett <andre.burkett@gmail.com>
2017-12-05 21:19:26 -0700

    miners build when no container exists

--- src/task.mine.ts
+++ src/task.mine.ts
@@ -5,6 +5,8 @@ var taskMine = {
         if (!creep.memory.taskQ) {
             if (creep.pos.findInRange(FIND_STRUCTURES, 3, {filter: (s: Structure) => s.structureType == STRUCTURE_CONTAINER}).length > 0)
                 creep.memory.taskQ = 'deposit';
+            else
+                creep.memory.taskQ = 'build';
         }
         if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
