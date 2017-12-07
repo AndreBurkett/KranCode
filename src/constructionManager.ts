121db5981fed0b73d5457e7b14599c77e3239291
AndreBurkett <andre.burkett@gmail.com>
2017-12-07 01:02:47 -0700

    debug

--- src/constructionManager.ts
+++ src/constructionManager.ts
@@ -28,12 +28,12 @@ export class architect implements constructionManager {
         for (let i in this.spawns) {
             if (this.r.controller){
                 let path = PathFinder.search(this.spawns[i].pos, this.r.controller.pos, { swampCost: 1, range: 2 })
-                this.r.memory.paths.controllerPath.push(path);
+                this.r.memory.paths.controllerPath[i] = path;
             }
         }
         for (let i in this.r.memory.paths.controllerPath) {
             for (let j in this.r.memory.paths.controllerPath.path) {
-                //new RoomVisual(this.r.name).circle(this.r.memory.paths.controllerPath[i].path[j].x, this.r.memory.paths.controllerPath[i].path[j].y);
+                new RoomVisual(this.r.name).circle(this.r.memory.paths.controllerPath[i].path[j].x, this.r.memory.paths.controllerPath[i].path[j].y);
             }
         }
     }
