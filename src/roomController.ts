06fd45266d85147ca4f4100a81fe7083caf94369
AndreBurkett <andre.burkett@gmail.com>
2017-12-07 11:21:16 -0700

    permanent deletion of legacy code

--- src/roomController.ts
+++ src/roomController.ts
@@ -63,40 +63,6 @@ function roomController(room: Room) {
     else if(room.controller) {
         room.createConstructionSite(room.controller.containerSpot[0], room.controller.containerSpot[1], STRUCTURE_CONTAINER);
     }
-/*
-    //Get Spawn to Source Containers Paths
-    if(!Memory.paths.sourceC || !Memory.paths.sourceC[numContainers]) {
-        let pathNum = 0;
-        for(let i in containers){
-            for(let j in spawns){
-                let startPos = containers[i].pos;
-                let endPos = spawns[j].pos;
-                let path = PathFinder.search(startPos, endPos, {swampCost: 1});
-                pathNum = pathNum + 1;
-                Memory.paths.sourceC[pathNum] = path;
-            }
-        }
-    }
-    else {
-        for(let i in Memory.paths.sourceC){
-            for(let j in Memory.paths.sourceC[i].path){
-                room.createConstructionSite(Memory.paths.sourceC[i].path[j].x, Memory.paths.sourceC[i].path[j].y, STRUCTURE_ROAD);
-            }
-        }
-    }
-    //Get Spawn to Controller Path
-    if(!Memory.paths.myPath && room.controller){
-        let startPos = room.controller.pos;
-        let endPos = room.find<StructureSpawn>(FIND_STRUCTURES, {filter: (s: Structure) => s.structureType == STRUCTURE_SPAWN});
-        for(let i in endPos){
-            let myPath = PathFinder.search(startPos, endPos[i].pos, {swampCost: 1})
-            Memory.paths.myPath = myPath;
-        }
-    }
-    //Place Spawn to Controller Roads
-    for(let sToC in Memory.paths.myPath.path) {
-        room.createConstructionSite(Memory.paths.myPath.path[sToC].x,Memory.paths.myPath.path[sToC].y, STRUCTURE_ROAD);
-    }*/
 
     ////////////////////////////////// Request New Creeps ///////////////////////////////////////
 
