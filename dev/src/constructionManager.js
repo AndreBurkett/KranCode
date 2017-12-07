821b0dd3be2241d78b1da60693314c66064650e7
AndreBurkett <andre.burkett@gmail.com>
2017-12-07 01:20:02 -0700

    bug fix

--- dev/src/constructionManager.js
+++ dev/src/constructionManager.js
@@ -23,7 +23,7 @@ class architect {
             }
         }
         for (let i in this.r.memory.paths.controllerPath) {
-            for (let j in this.r.memory.paths.controllerPath.path) {
+            for (let j in this.r.memory.paths.controllerPath[i].path) {
                 this.r.createConstructionSite(this.r.memory.paths.controllerPath[i].path[j].x, this.r.memory.paths.controllerPath[i].path[j].y, STRUCTURE_ROAD);
             }
         }
