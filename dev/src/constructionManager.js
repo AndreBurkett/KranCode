c5e62536d2104f747abca40228c6760a57967757
AndreBurkett <andre.burkett@gmail.com>
2017-12-07 03:26:47 -0700

    testing new container placement

--- dev/src/constructionManager.js
+++ dev/src/constructionManager.js
@@ -4,27 +4,47 @@ class architect {
     constructor(room) {
         this.spawns = room.find(FIND_STRUCTURES, { filter: (s) => s.structureType === STRUCTURE_SPAWN });
         this.r = room;
+        for (let i in room.sources) {
+            this.sources.push(Game.getObjectById(room.sources[i]));
+        }
         console.log('test');
     }
     createRoads() {
-        var sources = [];
         if (!this.r.memory.paths) {
             this.r.memory.paths = {};
             if (!this.r.memory.paths.controllerPath)
                 this.r.memory.paths.controllerPath = {};
         }
-        for (let i in this.r.sources) {
-            sources.push(Game.getObjectById(this.r.sources[i]));
-        }
         for (let i in this.spawns) {
             if (this.r.controller) {
-                let path = PathFinder.search(this.spawns[i].pos, this.r.controller.pos, { swampCost: 1, range: 2 });
+                let path = PathFinder.search(this.spawns[i].pos, this.r.controller.pos, { swampCost: 1, range: 2, ignoreRoads: true });
                 this.r.memory.paths.controllerPath[i] = path;
             }
         }
         for (let i in this.r.memory.paths.controllerPath) {
             for (let j in this.r.memory.paths.controllerPath[i].path) {
                 this.r.createConstructionSite(this.r.memory.paths.controllerPath[i].path[j].x, this.r.memory.paths.controllerPath[i].path[j].y, STRUCTURE_ROAD);
+            }
+        }
+    }
+    createSourceContainers() {
+        for (let i in this.sources) {
+            var target = [];
+            if (Game.map.getTerrainAt(this.sources[i].pos.x - 2, this.sources[i].pos.y, this.r.name) != 'wall') {
+                target.push(this.sources[i].pos.x - 2, this.sources[i].pos.y);
+            }
+            if (Game.map.getTerrainAt(this.sources[i].pos.x + 2, this.sources[i].pos.y, this.r.name) != 'wall') {
+                target.push(this.sources[i].pos.x + 2, this.sources[i].pos.y);
+            }
+            if (Game.map.getTerrainAt(this.sources[i].pos.x, this.sources[i].pos.y - 2, this.r.name) != 'wall') {
+                target.push(this.sources[i].pos.x, this.sources[i].pos.y - 2);
+            }
+            if (Game.map.getTerrainAt(this.sources[i].pos.x, this.sources[i].pos.y + 2, this.r.name) != 'wall') {
+                target.push(this.sources[i].pos.x, this.sources[i].pos.y + 2);
+            }
+            if (target.length > 0) {
+                var site = this.spawns[0].pos.findClosestByRange(target);
+                this.r.visual.circle(site);
             }
         }
     }
--- src/constructionManager.ts
+++ src/constructionManager.ts
@@ -6,28 +6,26 @@ interface constructionManager {
 export class architect implements constructionManager {
     r: Room;
     spawns: StructureSpawn[];
+    sources;
 
     public constructor(room: Room) {
         this.spawns = room.find<StructureSpawn>(FIND_STRUCTURES, { filter: (s: Structure) => s.structureType === STRUCTURE_SPAWN })
         this.r = room;
+        for (let i in room.sources) {
+            this.sources.push(Game.getObjectById(room.sources[i]))
+        }
         console.log('test');
     }
     public createRoads() {
-        var sources = [];
         if (!this.r.memory.paths) {
             this.r.memory.paths = {};
             if (!this.r.memory.paths.controllerPath)
                 this.r.memory.paths.controllerPath = {};
         }
 
-        for (let i in this.r.sources) {
-            sources.push(Game.getObjectById(this.r.sources[i]))
-        }
-
-
         for (let i in this.spawns) {
             if (this.r.controller){
-                let path = PathFinder.search(this.spawns[i].pos, this.r.controller.pos, { swampCost: 1, range: 2 })
+                let path = PathFinder.search(this.spawns[i].pos, this.r.controller.pos, { swampCost: 1, range: 2, ignoreRoads: true})
                 this.r.memory.paths.controllerPath[i] = path;
             }
         }
@@ -35,6 +33,28 @@ export class architect implements constructionManager {
             for (let j in this.r.memory.paths.controllerPath[i].path) {
                 //this.r.visual.circle(this.r.memory.paths.controllerPath[i].path[j].x, this.r.memory.paths.controllerPath[i].path[j].y);
                 this.r.createConstructionSite(this.r.memory.paths.controllerPath[i].path[j].x,this.r.memory.paths.controllerPath[i].path[j].y,STRUCTURE_ROAD)
+            }
+        }
+    }
+    public createSourceContainers(){
+        for(let i in this.sources){
+            var target = [];
+            if(Game.map.getTerrainAt(this.sources[i].pos.x -2, this.sources[i].pos.y, this.r.name) != 'wall'){
+                target.push(this.sources[i].pos.x -2, this.sources[i].pos.y)
+            }
+            if(Game.map.getTerrainAt(this.sources[i].pos.x +2, this.sources[i].pos.y, this.r.name) != 'wall'){
+                target.push(this.sources[i].pos.x +2, this.sources[i].pos.y)
+            }
+            if(Game.map.getTerrainAt(this.sources[i].pos.x, this.sources[i].pos.y - 2, this.r.name) != 'wall'){
+                target.push(this.sources[i].pos.x, this.sources[i].pos.y - 2)
+            }
+            if(Game.map.getTerrainAt(this.sources[i].pos.x, this.sources[i].pos.y + 2, this.r.name) != 'wall'){
+                target.push(this.sources[i].pos.x, this.sources[i].pos.y + 2)
+            }
+            if(target.length > 0){
+                var site = this.spawns[0].pos.findClosestByRange(target);
+                this.r.visual.circle(site);
+                //this.r.createConstructionSite(site,STRUCTURE_CONTAINER)
             }
         }
     }
