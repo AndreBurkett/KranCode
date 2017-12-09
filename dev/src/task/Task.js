03092770ec62a87feedb256450a5619e3dc7e986
AndreBurkett <andre.burkett@gmail.com>
2017-12-09 03:07:56 -0700

    flatten folders

--- dev/src/task/Task.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Task {
-    constructor(c) {
-        this.c = c;
-    }
-}
-exports.Task = Task;
-//# sourceMappingURL=Task.js.map
