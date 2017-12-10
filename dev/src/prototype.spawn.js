14e97d785c76e3b7a70ec1aa61618f9daa6d44be
AndreBurkett <andre.burkett@gmail.com>
2017-12-10 06:19:46 -0700

    beefed up pikemen slightly

--- dev/src/prototype.spawn.js
+++ dev/src/prototype.spawn.js
@@ -59,7 +59,7 @@ StructureSpawn.prototype.sCreep = function (role, specialty) {
             }
             break;
         case 'pikeman':
-            body.push(MOVE, ATTACK, MOVE, ATTACK);
+            body.push(MOVE, MOVE, ATTACK, MOVE, ATTACK, ATTACK);
             return this.spawnCreep(body, creepName.getName('Ap'), { memory: { role: 'pikeman', task: 'combatMelee' } });
             break;
     }
