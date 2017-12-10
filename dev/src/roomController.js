e9a887e83e82b0924c1fc9c58ae295a42d1777bf
AndreBurkett <andre.burkett@gmail.com>
2017-12-10 04:31:24 -0700

    fixed satMiner memory reference

--- dev/src/roomController.js
+++ dev/src/roomController.js
@@ -20,15 +20,15 @@ function roomController(room) {
             if (adjacentRoom[i]) {
                 if (Memory.rooms[adjacentRoom[i]]) {
                     if (Memory.rooms[adjacentRoom[i]].owner === 'Neutral') {
-                        if (Memory.rooms[adjacentRoom[i]].creeps && Memory.rooms[adjacentRoom[i]].creeps.satMiners) {
-                            if (Memory.rooms[adjacentRoom[i]].creeps.satMiners < 1) {
+                        if (Memory.rooms[adjacentRoom[i]].creeps && Memory.rooms[adjacentRoom[i]].creeps['satMiners']) {
+                            if (Memory.rooms[adjacentRoom[i]].creeps['satMiners'] < 1) {
                                 let satMiners = room.find(FIND_MY_CREEPS, { filter: (c) => c.memory.specialty === 'satMiner' && !c.memory.targetRoom });
                                 if (satMiners.length > 0) {
                                     for (let j in satMiners) {
                                         satMiners[j].memory.task = 'Mine';
                                         satMiners[j].memory.targetRoom = adjacentRoom[i];
                                         delete satMiners[j].memory.taskQ;
-                                        Memory.rooms[adjacentRoom[i]].creeps.satMiners++;
+                                        Memory.rooms[adjacentRoom[i]].creeps['satMiners']++;
                                     }
                                 }
                                 else {
