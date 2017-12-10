7d02599a9e4a6179ab54bb8ca5998c5bffea895f
AndreBurkett <andre.burkett@gmail.com>
2017-12-10 04:11:18 -0700

    fix error bound

--- dev/src/Task.js
+++ dev/src/Task.js
@@ -10,7 +10,7 @@ class Task {
     }
     Move(room) {
         let target = new RoomPosition(25, 25, room);
-        if (this.c.pos.roomName == room && this.c.pos.x > 2 && this.c.pos.x < 48 && this.c.pos.y < 2 && this.c.pos.y < 48) {
+        if (this.c.pos.roomName == room && this.c.pos.x > 2 && this.c.pos.x < 48 && this.c.pos.y > 2 && this.c.pos.y < 48) {
             this.c.memory.state = STATE_TARGETING;
         }
         else {
